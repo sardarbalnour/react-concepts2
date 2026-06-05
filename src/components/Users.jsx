3869516176942d2c47912dd1626597a9777e92fa
Sardar balnour <balnour.sardar@gmail.com>
2026-06-05 12:15:12 +0330

    show fetch data in UI

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((json) => setUsers(json));
   }, []);
-  return <div>Users</div>;
+  
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Users;
