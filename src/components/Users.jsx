3ce91b704cbe7fbe9b2350ac9f9f3c854cd916ac
Sardar balnour <balnour.sardar@gmail.com>
2026-06-06 11:58:51 +0330

    dependncy updating lifecycle

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,32 +3,38 @@ import { useEffect, useState } from "react";
 function Users() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(false);
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      // try {
-      //   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      //   const json = await res.json();
-      //   setUsers(json);
-      // } catch (error) {
-      //   setError(true);
-      // }
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => setUsers(json))
-        .catch((error) => setError(true));
+      try {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        const json = await res.json();
+        setUsers(json);
+        console.log(json)
+      } catch (error) {
+        setError(true);
+      }
     };
     fetchData();
-  }, []);
+  }, [id]);
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Id"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
       {!users.length && !error && <h3>Loading...</h3>}
-      <ul>
+      {/* <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
-      </ul>
+      </ul> */}
       {error && <h4>Something went wrong !</h4>}
     </div>
   );
