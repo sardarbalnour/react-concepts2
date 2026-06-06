999cbf2d3cb36aca9f3bcdfba80ce5c60be321ee
Sardar balnour <balnour.sardar@gmail.com>
2026-06-06 12:07:19 +0330

    http requests in event

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -8,18 +8,21 @@ function Users() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(
-          `https://jsonplaceholder.typicode.com/users/${id}`
-        );
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/`);
         const json = await res.json();
         setUsers(json);
-        console.log(json)
       } catch (error) {
         setError(true);
       }
     };
     fetchData();
-  }, [id]);
+  }, []);
+
+  const searchHandler = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const json = await res.json();
+    console.log(json);
+  };
 
   return (
     <div>
@@ -29,12 +32,13 @@ function Users() {
         value={id}
         onChange={(e) => setId(e.target.value)}
       />
+      <button onClick={searchHandler}>search</button>
       {!users.length && !error && <h3>Loading...</h3>}
-      {/* <ul>
+      <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
-      </ul> */}
+      </ul>
       {error && <h4>Something went wrong !</h4>}
     </div>
   );
