95f3cf8272415f92ebcbf3b2ab22f87c75b3047d
Sardar balnour <balnour.sardar@gmail.com>
2026-06-23 11:48:45 +0330

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
-import Users from "./components/Users";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  console.log(state, action);
+
+  switch (action) {
+    case "Increase":
+      return state + 1;
+    case "Decrease":
+      return state - 1;
+    case "Reset":
+      return 0;
+    default:
+      throw new Error("Invalid action");
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  const increaseHandler = () => {
+    // setCount((count) => count + 1);
+    dispatch("Increase");
+  };
+
+  const decreaseHandler = () => {
+    // if (count < 1) return;
+    // setCount((count) => count - 1);
+    dispatch("Decrease");
+  };
+
+  const resetHandler = () => {
+    // setCount(0);
+    dispatch("Reset");
+  };
+
   return (
     <>
-      <Users />
+      <h1>useReduce hook </h1>
+      <p>{count}</p>
+      <button onClick={increaseHandler}>increase</button>
+      <button onClick={resetHandler}>reset</button>
+      <button onClick={decreaseHandler}>decrease</button>
     </>
   );
 }
 
 export default App;
-
-//lifting state up
-// passing function as props
-// passing jsx as props
-// passing component as props
-// props drilling
-// component composition
-// life cycles in react and useEffect
-// react strict mode
-// how react works
-// state scheduling
-// state batching
-// Review
-// Review
