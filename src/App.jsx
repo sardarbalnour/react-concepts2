47d6555c605f4639bd59c751d4291b3a30025ade
Sardar balnour <balnour.sardar@gmail.com>
2026-06-24 17:00:02 +0330

    initialState

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, number: 26 };
+
 const reducer = (state, action) => {
   console.log(state, action);
 
   switch (action.type) {
     case "Increase":
-      return state + 1;
+      return { ...state, count: state.count + 1 };
     case "IncreaseByAmount":
-      return state + action.payload;
+      return { ...state, count: state.count + action.payload };
     case "Decrease":
-      return state - 1;
+      return { ...state, count: state.count - 1 };
+    case "UP":
+      return { ...state, number: state.number + 15 };
     case "Reset":
-      return 0;
+      return { ...state, count: 0 };
     default:
       throw new Error("Invalid action");
   }
 };
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const increaseHandler = () => {
     dispatch({ type: "Increase" });
@@ -39,11 +43,13 @@ function App() {
   return (
     <>
       <h1>useReducer hook </h1>
-      <p>{count}</p>
+      <p>{state.count}</p>
       <button onClick={increaseHandler}>increase</button>
       <button onClick={increaseByAmountHandler}>increase by amount</button>
       <button onClick={resetHandler}>reset</button>
       <button onClick={decreaseHandler}>decrease</button>
+      <p>{state.number}</p>
+      <button onClick={() => dispatch({ type: "UP" })}>UP number</button>
     </>
   );
 }
