449a2a23d208eacdc01c6249136456a92bc46f93
Sardar balnour <balnour.sardar@gmail.com>
2026-07-02 11:00:32 +0330

    useNavigate

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,23 +1,29 @@
-import { Link, useSearchParams } from "react-router";
+import { Link, useNavigate, useSearchParams } from "react-router";
 import NavBar from "../components/NavBar";
 
-function ProductsPage() {
-  const products = [
-    { id: 1, name: "product 1" },
-    { id: 2, name: "product 2" },
-    { id: 3, name: "product 3" },
-    { id: 4, name: "product 4" },
-  ];
+const products = [
+  { id: 1, name: "product 1" },
+  { id: 2, name: "product 2" },
+  { id: 3, name: "product 3" },
+  { id: 4, name: "product 4" },
+];
 
+function ProductsPage() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
   const sortBy = searchParams.get("sortBy");
   const order = searchParams.get("order");
   console.log(searchParams);
-  console.log(searchParams.toString())
+  console.log(searchParams.toString());
   console.log({ order, sortBy });
 
   const sortHandler = () => {
     setSearchParams({ order: "desc", sortBy: "sale" });
+  };
+
+  const homeHandler = () => {
+    navigate("/");
   };
 
   return (
@@ -31,6 +37,7 @@ function ProductsPage() {
           </li>
         ))}
       </ul>
+      <button onClick={homeHandler}>Go home</button>
     </div>
   );
 }
